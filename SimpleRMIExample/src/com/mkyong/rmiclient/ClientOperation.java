54e2ede728e6b9f2b21f206df1e36537e65b1675
andrea-w <andrea.k.williams7@gmail.com>
2017-11-07 14:28:36 -0800

    Update ClientOperation.java
    Added features selection GUI

--- SimpleRMIExample/src/com/mkyong/rmiclient/ClientOperation.java
+++ SimpleRMIExample/src/com/mkyong/rmiclient/ClientOperation.java
@@ -6,6 +6,9 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JLabel;
 
 import com.mkyong.rmiinterface.RMIInterface;
 
@@ -15,9 +18,35 @@ public class ClientOperation {
 	public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
 		
 		look_up = (RMIInterface) Naming.lookup("//localhost/MyServer");
-		String txt = JOptionPane.showInputDialog("What is your name?");
-			
-		String response = look_up.helloTo(txt);
-		JOptionPane.showMessageDialog(null, response);
+
+		featuresSelection();			
+	}
+
+	public static void featuresSelection() {
+		final JPanel panel = new JPanel();
+		final JRadioButton buttonC = new JRadioButton("Confidentiality");
+		final JRadioButton buttonI = new JRadioButton("Integrity");
+		final JRadioButton buttonA = new JRadioButton("Authentication");
+		final JLabel label = new JLabel("Select your desired security features:");
+
+		panel.add(label);
+		panel.add(buttonC);
+		panel.add(buttonI);
+		panel.add(buttonA);
+
+		JOptionPane.showMessageDialog(null, panel);
+
+		String txt = "";
+		if (buttonC.isSelected()) {
+			txt += "Confidentiality ";
+		}
+		if (buttonI.isSelected()) {
+			txt += "Integrity ";	
+		}
+		if (buttonA.isSelected()) {
+			txt += "Authentication ";	
+		}
+
+		System.out.println("Client selected features: " + txt);
 	}
 }
