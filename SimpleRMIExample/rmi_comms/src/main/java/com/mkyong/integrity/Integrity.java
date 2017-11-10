ccf7c1034199b081192e101c4b52a2f85eeb4839
andrea-w <andrea.k.williams7@gmail.com>
2017-11-09 19:02:59 -0800

    Update verify sig function in Integrity.java

--- SimpleRMIExample/rmi_comms/src/main/java/com/mkyong/integrity/Integrity.java
+++ SimpleRMIExample/rmi_comms/src/main/java/com/mkyong/integrity/Integrity.java
@@ -20,7 +20,7 @@ public class Integrity  {
 	@params:
 		msg: byte[] of message to be signed
 	*/
-	public static byte[] signMessage(byte[] msg) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
+	public MessageSigThing signMessage(byte[] msg) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
 		try {
 			// generate Signature object
 			Signature dsa = Signature.getInstance("SHA256withDSA");		
@@ -34,7 +34,9 @@ public class Integrity  {
 
 			byte[] sig = dsa.sign();
 
-			return sig;
+			MessageSigThing mst = new MessageSigThing(msg, sig, dsa);
+
+			return mst;
 
 	
 		} catch(NoSuchAlgorithmException e) {
@@ -55,14 +57,17 @@ public class Integrity  {
 		sig: byte array containing message signature
 		dsa: Signature object
 	*/
-	public static boolean verifySig(byte[] data, byte[] sig, Signature dsa) throws SignatureException, InvalidKeyException {
-			// initialize object with public key
-			PublicKey pub = pair.getPublic();
-			
-			try {
+	public static boolean verifySig(MessageSigThing mst) throws SignatureException, InvalidKeyException {
+		// initialize object with public key
+		PublicKey pub = pair.getPublic();
+		
+		try {
+			Signature dsa = mst.getSignatureObject();	
 			dsa.initVerify(pub);
 	
 			// update and verify data
+			byte[] data = mst.getData();
+			byte[] sig = mst.getSig();
 			dsa.update(data);
 			boolean verifies = dsa.verify(sig);
 			System.out.println("Signature verifies: " + verifies);
@@ -70,7 +75,7 @@ public class Integrity  {
 			if (!verifies) {
 				JOptionPane.showMessageDialog(null, "Warning!\nMessage has been corrupted.");	
 			}
-			
+		
 			return verifies;
 		} catch(SignatureException e) {
 			System.out.println(e);
@@ -79,6 +84,30 @@ public class Integrity  {
 		}
 
 		return false;
+	}
+
+	public class MessageSigThing{
+		private byte[] data;
+		private byte[] sig;
+		private Signature dsa;
+
+		public MessageSigThing(byte[] data, byte[] sig, Signature dsa) {
+			this.data = data;
+			this.sig = sig;
+			this.dsa = dsa;
+		}
+
+		public Signature getSignatureObject() {
+			return dsa;
+		}
+
+		public byte[] getSig() {
+			return sig;
+		}
+
+		public byte[] getData() {
+			return data;
+		}
 	}
 }
 
